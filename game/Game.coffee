6740d258aaabaf2933dc5d35139fb7c63ce92cc7
unknown <cracker642@gmail.com>
2012-11-12 10:54:42 -0500

    fix stuff

--- game/Game.coffee
+++ game/Game.coffee
@@ -3,6 +3,12 @@ totalLoaded = 0
 manifest = []
 canvas = null
 stage = null
+tgrid = new Array()
+for i in [0..19]
+    tgrid[i] = new Array()
+    for j in [0..19]
+        tgrid[i][j] = false
+
 g = new Graphics()
 
 branchs = { 
@@ -13,7 +19,7 @@ branchs = {
                 "color":"blue"
                 "lines": [
                              {x:0, y:15, x2:7, y2:15}
-                            ,{x:7, y:5, x2:7, y2:16}
+                             {x:7, y:5, x2:7, y2:16}
                              {x:7, y:5, x2:20, y2:5}]}
             "anotherBranch": {
                 "color":"orange"
@@ -23,8 +29,11 @@ branchs = {
 init = () -> 
     canvas = document.getElementById('gameCanvas')
     stage = new Stage(canvas)
+    s1 = new Shape(g)
+    stage.addChild(s1)
 
     stage.mouseEventsEnabled = true
+    stage.canvas.onclick = handleClickity
 
     drawGrid()
     drawBranch()
@@ -35,7 +44,7 @@ init = () ->
     stage.update()
 
 handleClickity = () ->
-    drawSquare(stage.mouseX, stage.mouseY)
+    checkSquare(stage.mouseX, stage.mouseY)
 
 handleFileLoad = (e) ->
     console.log("handling")
@@ -66,37 +75,31 @@ addTitleView = () ->
 drawGrid = () ->
     x = 0
     t = 0
-    z = 50
-    n = 50
    
     g.setStrokeStyle(1).beginStroke("black").moveTo(0, 0).lineTo(1000, 0)
-    s1 = new Shape(g)
-    stage.addChild(s1)
+    
 
     g.setStrokeStyle(1).beginStroke("black").moveTo(0, 0).lineTo(0, 1000)
-    s2 = new Shape(g)
-    stage.addChild(s2)
+    #s2 = new Shape(g)
+    #stage.addChild(s2)
 
     while x < 20
-        g.setStrokeStyle(1).beginStroke("black").moveTo(0, z).lineTo(1000,z);
-        s = new Shape(g)
-        stage.addChild(s)
+        g.setStrokeStyle(1).beginStroke("black").moveTo(0, x*50).lineTo(1000,x*50);
+        #s = new Shape(g)
+        #stage.addChild(s)
         x += 1
-        z += 50
 
     while t < 20
-        g.setStrokeStyle(1).beginStroke("black").moveTo(n, 0).lineTo(n,1000);
-        d = new Shape(g)
-        stage.addChild(d)
+        g.setStrokeStyle(1).beginStroke("black").moveTo(t*50, 0).lineTo(t*50,1000);
+        #d = new Shape(g)
+        #stage.addChild(d)
         t += 1
-        n += 50
 
 notLegalToDraw = (locx, locy) ->
     yMouse = Math.floor(locy/50)
     xMouse = Math.floor(locx/50)
     for name, path of branchs
-        lines = path.lines
-        for index, line of lines
+        for index, line of path.lines
             if xMouse >= line.x and xMouse <= line.x2 and yMouse >= line.y and yMouse <= line.y2
                 return true
                 break
@@ -107,30 +110,39 @@ enemyOcupation = (wave) ->
     if wave is 1
         enemies += 1;
 
-drawSquare = (locx, locy) ->
+checkSquare = (locx, locy) ->
     yMouse = Math.floor(locy/50)
     xMouse = Math.floor(locx/50)
    
-    if !notLegalToDraw(locx, locy)
+    if !notLegalToDraw(locx, locy) and !tgrid[xMouse][yMouse]
         g.setStrokeStyle(1).beginStroke("yellow").beginFill("yellow").drawRoundRect(xMouse*50, yMouse*50, canvas.width / 20, canvas.height / 20, 0);
-        s = new Shape(g);
-        stage.addChild(s)
+        tgrid[xMouse][yMouse] = true
+        #s = new Shape(g);
+        #stage.addChild(s)
+    return true
 
+drawSquares = () ->
+    for i in [0..19]
+        for j in [0..19]
+            if tgrid[i][j]
+                g.setStrokeStyle(1).beginStroke("yellow").beginFill("yellow").drawRoundRect(i*50, j*50, canvas.width / 20, canvas.height / 20, 0);
+
+clearScreen = () ->
+    g.setStrokeStyle(1).beginStroke("White").beginFill("White").drawRect(0,0,canvas.width,canvas.height,0)
 
 
 drawBranch = () ->
     for name, path of branchs
         color = path.color
-        lines = path.lines
-        for index, line of lines
+        for index, line of path.lines
             if line.y != line.y2
                 g.setStrokeStyle(50).beginStroke(color).moveTo(line.x*50 +25, line.y*50).lineTo(line.x2*50+25,line.y2*50)
-                s = new Shape(g)
-                stage.addChild(s)
+                #s = new Shape(g)
+                #stage.addChild(s)
             else
                 g.setStrokeStyle(50).beginStroke(color).moveTo(line.x*50, line.y*50+25).lineTo(line.x2*50, line.y2*50+25)
-                s2 = new Shape(g)
-                stage.addChild(s2)
+                #s2 = new Shape(g)
+                #stage.addChild(s2)
 
 addGameView = () ->
 
@@ -144,5 +156,8 @@ resetGame = () ->
     #reset dis shit
 
 tick = () ->
-    stage.canvas.onclick = handleClickity
+    #clearScreen()
+    #drawGrid()
+    #drawBranch()
+    #drawSquares()
     stage.update()
--- game/Game.js
+++ game/Game.js
@@ -1,8 +1,15 @@
-var addGameView, addTitleView, branchs, canvas, drawBranch, drawGrid, drawSquare, enemyOcupation, g, handleClickity, handleFileLoad, handleLoadComplete, init, manifest, notLegalToDraw, resetGame, stage, startGame, tick, totalLoaded;
+var addGameView, addTitleView, branchs, canvas, checkSquare, clearScreen, drawBranch, drawGrid, drawSquares, enemyOcupation, g, handleClickity, handleFileLoad, handleLoadComplete, i, init, j, manifest, notLegalToDraw, resetGame, stage, startGame, tgrid, tick, totalLoaded;
 totalLoaded = 0;
 manifest = [];
 canvas = null;
 stage = null;
+tgrid = new Array();
+for (i = 0; i <= 19; i++) {
+  tgrid[i] = new Array();
+  for (j = 0; j <= 19; j++) {
+    tgrid[i][j] = false;
+  }
+}
 g = new Graphics();
 branchs = {
   "mainBranch": {
@@ -50,9 +57,13 @@ branchs = {
   }
 };
 init = function() {
+  var s1;
   canvas = document.getElementById('gameCanvas');
   stage = new Stage(canvas);
+  s1 = new Shape(g);
+  stage.addChild(s1);
   stage.mouseEventsEnabled = true;
+  stage.canvas.onclick = handleClickity;
   drawGrid();
   drawBranch();
   Ticker.setFPS(60);
@@ -60,7 +71,7 @@ init = function() {
   return stage.update();
 };
 handleClickity = function() {
-  return drawSquare(stage.mouseX, stage.mouseY);
+  return checkSquare(stage.mouseX, stage.mouseY);
 };
 handleFileLoad = function(e) {
   var img;
@@ -93,43 +104,31 @@ addTitleView = function() {
   return stage.update();
 };
 drawGrid = function() {
-  var d, n, s, s1, s2, t, x, z, _results;
+  var t, x, _results;
   x = 0;
   t = 0;
-  z = 50;
-  n = 50;
   g.setStrokeStyle(1).beginStroke("black").moveTo(0, 0).lineTo(1000, 0);
-  s1 = new Shape(g);
-  stage.addChild(s1);
   g.setStrokeStyle(1).beginStroke("black").moveTo(0, 0).lineTo(0, 1000);
-  s2 = new Shape(g);
-  stage.addChild(s2);
   while (x < 20) {
-    g.setStrokeStyle(1).beginStroke("black").moveTo(0, z).lineTo(1000, z);
-    s = new Shape(g);
-    stage.addChild(s);
+    g.setStrokeStyle(1).beginStroke("black").moveTo(0, x * 50).lineTo(1000, x * 50);
     x += 1;
-    z += 50;
   }
   _results = [];
   while (t < 20) {
-    g.setStrokeStyle(1).beginStroke("black").moveTo(n, 0).lineTo(n, 1000);
-    d = new Shape(g);
-    stage.addChild(d);
-    t += 1;
-    _results.push(n += 50);
+    g.setStrokeStyle(1).beginStroke("black").moveTo(t * 50, 0).lineTo(t * 50, 1000);
+    _results.push(t += 1);
   }
   return _results;
 };
 notLegalToDraw = function(locx, locy) {
-  var index, line, lines, name, path, xMouse, yMouse;
+  var index, line, name, path, xMouse, yMouse, _ref;
   yMouse = Math.floor(locy / 50);
   xMouse = Math.floor(locx / 50);
   for (name in branchs) {
     path = branchs[name];
-    lines = path.lines;
-    for (index in lines) {
-      line = lines[index];
+    _ref = path.lines;
+    for (index in _ref) {
+      line = _ref[index];
       if (xMouse >= line.x && xMouse <= line.x2 && yMouse >= line.y && yMouse <= line.y2) {
         return true;
         break;
@@ -143,29 +142,47 @@ enemyOcupation = function(wave) {
     return enemies += 1;
   }
 };
-drawSquare = function(locx, locy) {
-  var s, xMouse, yMouse;
+checkSquare = function(locx, locy) {
+  var xMouse, yMouse;
   yMouse = Math.floor(locy / 50);
   xMouse = Math.floor(locx / 50);
-  if (!notLegalToDraw(locx, locy)) {
+  if (!notLegalToDraw(locx, locy) && !tgrid[xMouse][yMouse]) {
     g.setStrokeStyle(1).beginStroke("yellow").beginFill("yellow").drawRoundRect(xMouse * 50, yMouse * 50, canvas.width / 20, canvas.height / 20, 0);
-    s = new Shape(g);
-    return stage.addChild(s);
+    tgrid[xMouse][yMouse] = true;
   }
+  return true;
+};
+drawSquares = function() {
+  var i, j, _results;
+  _results = [];
+  for (i = 0; i <= 19; i++) {
+    _results.push((function() {
+      var _results2;
+      _results2 = [];
+      for (j = 0; j <= 19; j++) {
+        _results2.push(tgrid[i][j] ? g.setStrokeStyle(1).beginStroke("yellow").beginFill("yellow").drawRoundRect(i * 50, j * 50, canvas.width / 20, canvas.height / 20, 0) : void 0);
+      }
+      return _results2;
+    })());
+  }
+  return _results;
+};
+clearScreen = function() {
+  return g.setStrokeStyle(1).beginStroke("White").beginFill("White").drawRect(0, 0, canvas.width, canvas.height, 0);
 };
 drawBranch = function() {
-  var color, index, line, lines, name, path, s, s2, _results;
+  var color, index, line, name, path, _results;
   _results = [];
   for (name in branchs) {
     path = branchs[name];
     color = path.color;
-    lines = path.lines;
     _results.push((function() {
-      var _results2;
+      var _ref, _results2;
+      _ref = path.lines;
       _results2 = [];
-      for (index in lines) {
-        line = lines[index];
-        _results2.push(line.y !== line.y2 ? (g.setStrokeStyle(50).beginStroke(color).moveTo(line.x * 50 + 25, line.y * 50).lineTo(line.x2 * 50 + 25, line.y2 * 50), s = new Shape(g), stage.addChild(s)) : (g.setStrokeStyle(50).beginStroke(color).moveTo(line.x * 50, line.y * 50 + 25).lineTo(line.x2 * 50, line.y2 * 50 + 25), s2 = new Shape(g), stage.addChild(s2)));
+      for (index in _ref) {
+        line = _ref[index];
+        _results2.push(line.y !== line.y2 ? g.setStrokeStyle(50).beginStroke(color).moveTo(line.x * 50 + 25, line.y * 50).lineTo(line.x2 * 50 + 25, line.y2 * 50) : g.setStrokeStyle(50).beginStroke(color).moveTo(line.x * 50, line.y * 50 + 25).lineTo(line.x2 * 50, line.y2 * 50 + 25));
       }
       return _results2;
     })());
@@ -179,6 +196,5 @@ addGameView = function() {
 startGame = function() {};
 resetGame = function() {};
 tick = function() {
-  stage.canvas.onclick = handleClickity;
   return stage.update();
 };
